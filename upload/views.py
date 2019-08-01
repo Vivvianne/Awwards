bd38250e2ddaf6775b98f3dab82ccc571ed425be
Vivvianne Kimani <ndiahav@gmail.com>
2019-08-01 14:52:24 +0300

    added some place holders in our views for our templates, and added styling in the static folder

--- upload/views.py
+++ upload/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'decription': 'First post content',
+        'link': 'Project1',
+        'date_posted': 'August 27, 2018',
+        'comments':'others comments'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'decription': 'First post content',
+        'link': 'Project1',
+        'date_posted': 'August 28, 2018',
+        'comments':'other-peoples comments'
+    }
+]
 
 def home(request):
-    return render(request, 'upload/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'upload/home.html', context)
 
 def about(request):
     return render(request, 'upload/about.html')
