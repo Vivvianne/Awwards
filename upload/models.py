79ffe70b8efdda2ceead349d37e8365e31bc4993
Vivvianne Kimani <ndiahav@gmail.com>
2019-08-02 18:34:21 +0300

    debugged home.html

--- upload/models.py
+++ upload/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -10,6 +11,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     
+    
     @classmethod
     def search_by_title(cls,search_term):
         post = cls.objects.filter(title__icontains=search_term)
@@ -17,6 +19,15 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        if img.height  > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save()
     
     
     
